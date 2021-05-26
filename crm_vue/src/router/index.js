eb0391a0852e3e1127e2e2b651c67209cdde11b4
Jonas <j.g.f.pettersson@gmail.com>
2021-05-26 13:34:25 +0200

    dashboard

--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import Dashboard from '../views/dashboard/Dashboard.vue'
 
 const routes = [
   {
@@ -27,6 +28,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
