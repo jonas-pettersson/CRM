4525230a2dadbf7066776d99675ed3a4c38a04e7
Jonas <j.g.f.pettersson@gmail.com>
2021-05-26 10:22:41 +0200

    Signup page

---
+++ crm_vue/src/views/SignUp.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-4 is-offset-4">
+        <h1 class="title">Sign up</h1>
+
+        <form action="">
+          <div class="field">
+            <label for="email">Email</label>
+            <div class="control">
+              <input type="email" name="email" id="email" class="input" />
+            </div>
+          </div>
+
+          <div class="field">
+            <label for="password1">Password</label>
+            <div class="control">
+              <input
+                type="password"
+                name="password1"
+                id="password1"
+                class="input"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label for="password2">Repeat password</label>
+            <div class="control">
+              <input
+                type="password"
+                name="password2"
+                id="password2"
+                class="input"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+}
+</script>
