e1d51410d191862d6c65a2c4211e760dc6b2a4e8
Jonas <j.g.f.pettersson@gmail.com>
2021-05-26 16:40:55 +0200

    set up router guards

--- crm_vue/src/App.vue
+++ crm_vue/src/App.vue
@@ -10,10 +10,22 @@
 
 <script>
 import Navbar from '@/components/layout/Navbar'
+import axios from 'axios'
+
 export default {
   name: 'App',
   components: {
     Navbar,
+  },
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+
+    if (this.$store.state.token) {
+      axios.defaults.headers.common['Authorization'] =
+        'Token ' + this.$store.state.token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ''
+    }
   },
 }
 </script>
--- crm_vue/src/router/index.js
+++ crm_vue/src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import Dashboard from '../views/dashboard/Dashboard.vue'
+import MyAccount from '../views/dashboard/MyAccount.vue'
 
 const routes = [
   {
@@ -32,13 +35,35 @@ const routes = [
   {
     path: '/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: Dashboard,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some(record => record.meta.requireLogin) &&
+    !store.state.isAuthenticated
+  ) {
+    next('/log-in')
+  } else {
+    next()
+  }
 })
 
 export default router
