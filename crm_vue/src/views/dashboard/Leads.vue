c506056586c8e116c1bfc97a0f943f2cf0f02844
Jonas <j.g.f.pettersson@gmail.com>
2021-06-14 15:02:10 +0200

    show assigned to

--- crm_vue/src/views/dashboard/Leads.vue
+++ crm_vue/src/views/dashboard/Leads.vue
@@ -13,12 +13,18 @@
             <tr>
               <th>Company</th>
               <th>Contact person</th>
+              <th>Assigned to</th>
               <th>Status</th>
               <th></th>
             </tr>
             <tr v-for="lead in leads" :key="lead.id">
               <td>{{ lead.company }}</td>
               <td>{{ lead.contact_person }}</td>
+              <td>
+                <template v-if="lead.assigned_to"
+                  >{{ lead.assigned_to.username }}
+                </template>
+              </td>
               <td>{{ lead.status }}</td>
               <td>
                 <router-link :to="{ name: 'Lead', params: { id: lead.id } }"
